bf11bd3f7f7b20630fa57921d9523f2c60029153
JosefHampp <s8johamp@stud.uni-saarland.de>
2019-10-06 17:23:02 +0200

    First try to visualize a pitch

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Automatic detection of soccer team formations for various game phases
 Neues Repository um den aktuellen Stand meiner Thesis zu speichern.
 Ordner mit den XML-Files befinden sich im selben Order wie das Git-Projekt.
 
+Für die Visualisierung wurde auf das Tutorial von [Towards Data Science](https://towardsdatascience.com/advanced-sports-visualization-with-pandas-matplotlib-and-seaborn-9c16df80a81b) zurückgriffen.
+
 Projekt
 ├── 19-06-12_Mainz_GER_EST Sportec Data
 │   ├── DFB_KPI_Merged.xlsx
--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import os.path
 
-#XML wird geladen
-
-#tree = ET.parse('DATA_STS/position_half.xml')
+# XML wird geladen
 tree = ET.parse(os.path.dirname(__file__) + '/../Data_STS/DFL_04_02_positions_raw_DFL-COM-000001_DFL-MAT-X03BWS.xml')
 xmlPlayer = ET.parse(os.path.dirname(__file__) + '/../Data_STS/DFL_01_05_masterdata_DFL-CLU-000N99_DFL-SEA-0001K4_player.xml')
 root = tree.getroot()
@@ -13,11 +11,8 @@ rootPlayer = xmlPlayer.getroot()
 
 #<FrameSet GameSection="firstHalf" MatchId="DFL-MAT-X03BWS" TeamId="DFL-CLU-000N9A" PersonId="DFL-OBJ-002G6R">
 #<Object ObjectId="DFL-OBJ-002G6N" Type="player" Name="Frenkie de Jong" FirstName="Frenkie" LastName="de Jong" BirthDate="12.05.1997" CountryOfBirthGerman="Niederlande" CountryOfBirthEnglish="Netherlands" CountryOfBirthSpanish="Holanda" NationalityGerman="Niederländisch" NationalityEnglish="Dutch" NationalitySpanish="neerlandés" Height="180" Weight="74" ShirtNumber="21" PlayingPositionGerman="Mittelfeld" PlayingPositionEnglish="midField" PlayingPositionSpanish="medio campo" ClubId="DFL-CLU-000N9A" ClubName="Niederlande Nationalmannschaft"/>
-
-
-
 #<Objects FeedType="player">
-#        <Object ObjectId="DFL-OBJ-0000O3" Type="player" Name="Marco Reus" FirstName="Marco" LastName="Reus" BirthDate="31.05.1989" BirthPlace="Dortmund" CountryOfBirthGerman="Deutschland" CountryOfBirthEnglish="Germany" CountryOfBirthSpanish="Alemania" NationalityGerman="Deutsch" NationalityEnglish="German" NationalitySpanish="alemán" Height="180" Weight="71" ShirtNumber="11" PlayingPositionGerman="Mittelfeld" PlayingPositionEnglish="midField" PlayingPositionSpanish="medio campo" ClubId="DFL-CLU-000N99" ClubName="Deutschland Nationalmannschaft"/>
+#<Object ObjectId="DFL-OBJ-0000O3" Type="player" Name="Marco Reus" FirstName="Marco" LastName="Reus" BirthDate="31.05.1989" BirthPlace="Dortmund" CountryOfBirthGerman="Deutschland" CountryOfBirthEnglish="Germany" CountryOfBirthSpanish="Alemania" NationalityGerman="Deutsch" NationalityEnglish="German" NationalitySpanish="alemán" Height="180" Weight="71" ShirtNumber="11" PlayingPositionGerman="Mittelfeld" PlayingPositionEnglish="midField" PlayingPositionSpanish="medio campo" ClubId="DFL-CLU-000N99" ClubName="Deutschland Nationalmannschaft"/>
 players = []
 for player in rootPlayer.iter('Object'):
     players.append([player.get('ObjectId'), player.get('Name')])
